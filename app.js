2a2b92440f73311196e1eef66572645fa814f3aa
chidoriii7 <muratore.danilo@yahoo.it>
2024-05-14 16:35:46 +0200

    js-our-team : milestone 1

---
+++ app.js
@@ -0,0 +1,61 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede
+
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+const ourteam = ['Wayne Barnett', 'Angela Caroll', 'Walter Gordon', 'Angela Lopez', 'Scott Estrada', 'Barbara Ramos',];
+
+const wayneBarnett = {
+    name : 'Wayne Barnett',
+    role : 'Founder & CEO',
+    photo: 'wayne-barnett-fouder-ceo.jps'
+}
+
+const angelaCaroll = {
+    name : 'Angela Caroll',
+    role : 'Chief Editor',
+    photo: 'angela-caroll-chief-editor.jps'
+}
+
+const walterGordon = {
+    name : 'Walter Gordon',
+    role : 'Office Manager',
+    photo: 'walter-gordon-office-manager.jps'
+}
+
+const angelaLopez = {
+    name : 'Angela Lopez',
+    role : 'Social Media Manager',
+    photo: 'angela-lopez-social-media-manager.jps'
+}
+
+const scottEstrada = {
+    name : 'Scott Estrada',
+    role : 'Developer',
+    photo: 'scott-estrada-developer.jps'
+}
+
+const barbaraRamos = {
+    name : 'Barbara Ramos',
+    role : 'Graphic Designer',
+    photo: 'barbara-ramos-graphic-designer.jps'
+}
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+console.log(wayneBarnett);
+console.log(angelaCaroll);
+console.log(walterGordon);
+console.log(angelaLopez);
+console.log(scottEstrada);
+console.log(barbaraRamos);
+
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
